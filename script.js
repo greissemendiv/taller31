614ed8e9cc09856d2862148201ae9b4551661868
Greisse Mendivelso <est.greisse.mendiv@unimilitar.edu.co>
2026-05-08 12:41:08 -0500

    Escenas

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const scenes = [
+
+    // Dentro
+    {x1:150, y1:150, x2:300, y2:250},
+
+    // Fuera
+    {x1:20, y1:20, x2:80, y2:80},
+
+    // Entrando
+    {x1:50, y1:150, x2:200, y2:200},
+
+    // Saliendo
+    {x1:200, y1:200, x2:500, y2:350},
+
+    // Cruzando
+    {x1:50, y1:50, x2:500, y2:400}
+];
+
+let currentScene = 0;
+
 function toCanvasY(y){
     return canvas.height - y;
 }
@@ -50,20 +70,48 @@ function drawViewport(xmin, ymin, xmax, ymax){
     drawLine(xmin, ymax, xmin, ymin, "yellow");
 }
 
-function drawAxes(){
+function renderScene(){
 
-    drawLine(0,0,canvas.width,0,"white");
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    drawLine(0,0,0,canvas.height,"white");
+    const xmin = parseInt(document.getElementById("xmin").value);
+    const ymin = parseInt(document.getElementById("ymin").value);
+    const xmax = parseInt(document.getElementById("xmax").value);
+    const ymax = parseInt(document.getElementById("ymax").value);
+
+    drawViewport(xmin,ymin,xmax,ymax);
+
+    const line = scenes[currentScene];
+
+    drawLine(
+        line.x1,
+        line.y1,
+        line.x2,
+        line.y2,
+        "red"
+    );
 }
 
-drawAxes();
+document.getElementById("nextBtn").onclick = () => {
 
-const xmin = parseInt(document.getElementById("xmin").value);
-const ymin = parseInt(document.getElementById("ymin").value);
-const xmax = parseInt(document.getElementById("xmax").value);
-const ymax = parseInt(document.getElementById("ymax").value);
+    currentScene++;
 
-drawViewport(xmin, ymin, xmax, ymax);
+    if(currentScene >= scenes.length){
+        currentScene = 0;
+    }
 
-drawLine(50,50,300,200,"red");
+    renderScene();
+};
+
+document.getElementById("prevBtn").onclick = () => {
+
+    currentScene--;
+
+    if(currentScene < 0){
+        currentScene = scenes.length - 1;
+    }
+
+    renderScene();
+};
+
+renderScene();
