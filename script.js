5f0162e493e4ef3208f277447e42eecf7659ab56
Greisse Mendivelso <est.greisse.mendiv@unimilitar.edu.co>
2026-05-08 11:54:24 -0500

    Sistema cartesiano

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+function toCanvasY(y){
+    return canvas.height - y;
+}
+
+function drawAxes(){
+
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 1;
+
+    // Eje X
+    ctx.beginPath();
+    ctx.moveTo(0, toCanvasY(0));
+    ctx.lineTo(canvas.width, toCanvasY(0));
+    ctx.stroke();
+
+    // Eje Y
+    ctx.beginPath();
+    ctx.moveTo(0, toCanvasY(0));
+    ctx.lineTo(0, toCanvasY(canvas.height));
+    ctx.stroke();
+}
+
+drawAxes();
